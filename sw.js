49f33d2676ea29556607330bbe9cbd2829a0788d
daskaos23 <daskaos23@googlemail.com>
2026-07-30 10:24:32 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Share-Target ("Teilen mit -> K-OS") legt Dateien in IndexedDB ab und
    oeffnet die Files-Linse. Dropbox-/Meta-Aufrufe werden NIE gecacht. */
 
-const CACHE = 'kos-suite-v6';
+const CACHE = 'kos-suite-v7';
 const SHELL = [
   './',
   './index.html',
